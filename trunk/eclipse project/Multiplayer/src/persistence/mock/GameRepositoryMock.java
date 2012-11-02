edda920687c11ed03112d63e6ebf63e7ded91474
endrebalogh88@gmail.com <endrebalogh88@gmail.com@c9d7cdea-e0bc-95f3-4048-889a8add2bef>
2012-11-02 13:04:15 +0000

    Added Reversi to game repository. Single player is now enabled

--- trunk/eclipse project/Multiplayer/src/persistence/mock/GameRepositoryMock.java
+++ trunk/eclipse project/Multiplayer/src/persistence/mock/GameRepositoryMock.java
@@ -16,10 +16,18 @@ public class GameRepositoryMock implements GameRepository {
 		x0.setGameId("x-0");
 		x0.setShortName("X and 0");
 		x0.setName("Tic-tac-toe - X and 0");
-		String description = "Tic-tac-toe, originally called noughts and crosses (and still known as this in Britain and Australia), Xs and Os (in Ireland) and X and 0 (in India) is a pencil-and-paper game for two players, X and O, who take turns marking the spaces in a 3x3 grid. The player who succeeds in placing three respective marks in a horizontal, vertical, or diagonal row wins the game.";
-		x0.setDescription(description);
+		String x0Description = "Tic-tac-toe, originally called noughts and crosses (and still known as this in Britain and Australia), Xs and Os (in Ireland) and X and 0 (in India) is a pencil-and-paper game for two players, X and O, who take turns marking the spaces in a 3x3 grid. The player who succeeds in placing three respective marks in a horizontal, vertical, or diagonal row wins the game.";
+		x0.setDescription(x0Description);
+		
+		Game reversi = new Game();
+		reversi.setGameId("reversi");
+		reversi.setShortName("Reversi");
+		reversi.setName("Reversi");
+		String reversiDescription = "Reversi is a strategy board game for two players, played on an 8x8 uncheckered board. There are 64 identical pieces called 'disks' (often spelled 'discs'), which are light on one side and dark on the other to correspond with the opponents in a game. Players take alternate turns by placing a piece of their own color on the board, in such a position that there exists at least one straight (horizontal, vertical, or diagonal) occupied line between the new piece and another self-owned piece, with one or more contiguous pieces of the opponent between them, which are thus 'captured'. Each player's objective is generally to have as many disks one's own color at the end as possible and for one's opponent to have as few.";
+		reversi.setDescription(reversiDescription);
 		
 		games.add(x0);
+		games.add(reversi);
 	}
 
 	@Override
