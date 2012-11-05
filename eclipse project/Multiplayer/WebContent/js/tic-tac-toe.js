eec50895fb6f1e8a2a7effb6b6f8f3cee23fe72b
endrebalogh88 <endrebalogh88@gmail.com>
2012-11-05 08:59:55 +0000

    refactored board logic functions into an object

--- eclipse project/Multiplayer/WebContent/js/tic-tac-toe.js
+++ eclipse project/Multiplayer/WebContent/js/tic-tac-toe.js
@@ -3,6 +3,7 @@ angular.module('tic-tac-toe', []);
 
 function TicTacToeController($scope, $http) {
 	var controller = this;
+	var boardLogic = null;
 
 	this.initSubscription($scope);
 	this.initGameOverDialogMethods($scope, $http);
@@ -14,12 +15,13 @@ function TicTacToeController($scope, $http) {
 	// setup board
 	$scope.resetBoard = function() {
 		$scope.board = controller.createBoard(boardSize);
+		boardLogic = new BoardLogic($scope.board);
 		$scope.currentPlayerIndex = 0;
 		delete $scope.winner;
 		delete $scope.opponentsLastMove;
 	};
 	$scope.resetBoard();
-
+	
 	// click on a cell
 	$scope.placeSymbol = function(row, column) {
 		// if move not legal 
@@ -40,11 +42,11 @@ function TicTacToeController($scope, $http) {
 	};
 
 	$scope.checkVictory = function() {
-		if (currentPlayerIsVictorious($scope.board)) {
+		if (boardLogic.currentPlayerIsVictorious()) {
 			$scope.winner = $scope.players[$scope.currentPlayerIndex];
 			$scope.victoryText = $scope.winner + " wins!";
 			controller.displayVictoryDialog();
-		} else if (boardIsFull($scope.board)) {
+		} else if (boardLogic.boardIsFull()) {
 			$scope.winner = 'noone';
 			$scope.victoryText = 'Draw!';
 			controller.displayVictoryDialog();
@@ -60,90 +62,6 @@ function TicTacToeController($scope, $http) {
 			this.currentPlayerIndex = 0;
 	};
 
-	// TODO refactor functions into board object
-	
-	function currentPlayerIsVictorious(board) {
-		var length = board.length;
-
-		// check rows
-		for ( var i = 0; i < length; i++) {
-			var row = board[i];
-			// check against first value
-			var firstValueInRow = row[0].content;
-			// if nothing in first cell, the row can't be full
-			if (!firstValueInRow)
-				continue;
-			// go through the cells in the row
-			var rowFullSame = true;
-			for ( var c = 1; c < length && rowFullSame; c++) {
-				var cell = row[c];
-				if (cell.content != firstValueInRow) {
-					rowFullSame = false;
-				}
-			}
-			if (rowFullSame)
-				return true;
-		}
-
-		// check columns
-		for ( var columnCounter = 0; columnCounter < length; columnCounter++) {
-			// check against first value
-			var firstValueInColumn = board[0][columnCounter].content;
-			// if nothing in first cell, the row can't be full
-			if (!firstValueInColumn)
-				continue;
-			// go through the cells in the column
-			var columnFullSame = true;
-			for ( var rowCounter = 1; rowCounter < length && columnFullSame; rowCounter++) {
-				var cell = board[rowCounter][columnCounter];
-				if (cell.content != firstValueInColumn) {
-					columnFullSame = false;
-				}
-			}
-			if (columnFullSame)
-				return true;
-		}
-
-		// check descending (first) diagonal
-		var firstValueInDescendingDiagonal = board[0][0].content;
-		if (firstValueInDescendingDiagonal) {
-			var diagonalSame = true;
-			for ( var diagonalCounter = 1; diagonalCounter < length && diagonalSame; diagonalCounter++) {
-				var cell = board[diagonalCounter][diagonalCounter];
-				if (cell.content != firstValueInDescendingDiagonal) {
-					diagonalSame = false;
-				}
-			}
-			if (diagonalSame)
-				return true;
-		}
-
-		// check descending (second) diagonal
-		var firstValueInAscendingDiagonal = board[0][length - 1].content;
-		if (firstValueInAscendingDiagonal) {
-			var diagonalSame = true;
-			for ( var rowCounter = 1, columnCounter = length - 2; rowCounter < length && diagonalSame; rowCounter++, columnCounter--) {
-				var cell = board[rowCounter][columnCounter];
-				if (cell.content != firstValueInAscendingDiagonal) {
-					diagonalSame = false;
-				}
-			}
-			if (diagonalSame)
-				return true;
-		}
-
-		return false;
-	};
-
-	function boardIsFull(board) {
-		for ( var i = 0; i < board.length; i++) {
-			for ( var j = 0; j < board.length; j++) {
-				if (!board[i][j].content)
-					return false;
-			}
-		}
-		return true;
-	};
 }
 
 TicTacToeController.prototype = new AngularController();
@@ -171,3 +89,91 @@ TicTacToeController.prototype.opponentMoves = function($scope, move){
 	});
 };
 
+function BoardLogic(board){
+	this.board = board;
+}
+
+BoardLogic.prototype.boardIsFull = function() {
+	var board = this.board;
+	for ( var i = 0; i < board.length; i++) {
+		for ( var j = 0; j < board.length; j++) {
+			if (!board[i][j].content)
+				return false;
+		}
+	}
+	return true;
+};
+
+BoardLogic.prototype.currentPlayerIsVictorious = function () {
+	var board = this.board;
+	var length = board.length;
+
+	// check rows
+	for ( var i = 0; i < length; i++) {
+		var row = board[i];
+		// check against first value
+		var firstValueInRow = row[0].content;
+		// if nothing in first cell, the row can't be full
+		if (!firstValueInRow)
+			continue;
+		// go through the cells in the row
+		var rowFullSame = true;
+		for ( var c = 1; c < length && rowFullSame; c++) {
+			var cell = row[c];
+			if (cell.content != firstValueInRow) {
+				rowFullSame = false;
+			}
+		}
+		if (rowFullSame)
+			return true;
+	}
+
+	// check columns
+	for ( var columnCounter = 0; columnCounter < length; columnCounter++) {
+		// check against first value
+		var firstValueInColumn = board[0][columnCounter].content;
+		// if nothing in first cell, the row can't be full
+		if (!firstValueInColumn)
+			continue;
+		// go through the cells in the column
+		var columnFullSame = true;
+		for ( var rowCounter = 1; rowCounter < length && columnFullSame; rowCounter++) {
+			var cell = board[rowCounter][columnCounter];
+			if (cell.content != firstValueInColumn) {
+				columnFullSame = false;
+			}
+		}
+		if (columnFullSame)
+			return true;
+	}
+
+	// check descending (first) diagonal
+	var firstValueInDescendingDiagonal = board[0][0].content;
+	if (firstValueInDescendingDiagonal) {
+		var diagonalSame = true;
+		for ( var diagonalCounter = 1; diagonalCounter < length && diagonalSame; diagonalCounter++) {
+			var cell = board[diagonalCounter][diagonalCounter];
+			if (cell.content != firstValueInDescendingDiagonal) {
+				diagonalSame = false;
+			}
+		}
+		if (diagonalSame)
+			return true;
+	}
+
+	// check descending (second) diagonal
+	var firstValueInAscendingDiagonal = board[0][length - 1].content;
+	if (firstValueInAscendingDiagonal) {
+		var diagonalSame = true;
+		for ( var rowCounter = 1, columnCounter = length - 2; rowCounter < length && diagonalSame; rowCounter++, columnCounter--) {
+			var cell = board[rowCounter][columnCounter];
+			if (cell.content != firstValueInAscendingDiagonal) {
+				diagonalSame = false;
+			}
+		}
+		if (diagonalSame)
+			return true;
+	}
+
+	return false;
+};
